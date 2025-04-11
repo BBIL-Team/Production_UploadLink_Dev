cc1e0121b858b45412f87adcd769c5e7124d73ae
BBIL-Team <deepshika5686@bharatbiotech.com>
2025-04-11 14:22:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
 
 function App() {
   const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
