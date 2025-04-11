d30fe73511dd6602f3e3ba4d76298c2144bf1c6b
BBIL-Team <deepshika5686@bharatbiotech.com>
2025-04-11 14:46:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
-import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-
-type Todo = {
-  id: string;
-  content: string;
-};
 
 function App() {
   const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data: any) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
-  }
 
   return (
-    <main>
+    <main style={{ padding: '2rem' }}>
       <button onClick={signOut}>Sign out</button>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <h1>Welcome, {user?.signInDetails?.loginId}</h1>
+      <p>🎉 App successfully hosted. You can start building your features now.</p>
+      <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+        Review next step of this tutorial
+      </a>
     </main>
   );
 }
